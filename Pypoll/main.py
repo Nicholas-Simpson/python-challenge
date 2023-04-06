1120647a194a667a71efa0898fc673b2b15ce7d7
Nicholas Simpson <114123854+Nicholas-Simpson@users.noreply.github.com>
2023-04-05 18:00:08 -0700

    finishing uploading completed documents eg (completed pypoll and readme update)

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -6,7 +6,8 @@ election_csv = os.path.join("Resources", "election_data.csv")
 
 #define variables we will use for calculations and data storage
 total_votes = 0
-poll_data = {"candidates": {"name":[],"vote_count":0}}
+candidates = {}
+
 
 with open(election_csv,encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile)
@@ -14,33 +15,40 @@ with open(election_csv,encoding="utf-8") as csvfile:
 
     for row in csvreader:
         total_votes += 1
+        if row[2] in candidates.keys():
+            candidates[row[2]] += 1 
+        else: candidates[row[2]] = 1
+         
+winner_count = 0
 
-        if row[2] not in poll_data["candidates"]["name"]:
-            poll_data["candidates"]["name"].append(row[2])
+for candidate in candidates.keys():
+    if candidates[candidate] > winner_count:
+        winner_count = candidates[candidate]
+        winner = candidate
 
-        if row[2] == poll_data["candidates"]["name"]:
-            poll_data["candidates"]["vote_count"] += 1
 
-winner = 0
 
-print(poll_data)
+print(f"Election Results\n"
+      f"------------------------------\n"
+      f"Total Votes: {total_votes}\n"
+      f"------------------------------\n")
 
-# print(f"Election Results\n"
-#       f"------------------------------\n"
-#       f"Total Votes: {total_votes}\n"
-#       f"------------------------------\n"
-#       f"{poll_data['candidates']['name']}: {poll_data['candidates']['vote_count']}% ({poll_data['candidates']['vote_count']})\n"
-#       f"------------------------------\n"
-#       f"Winner: {winner}\n"
-#       f"------------------------------\n")
+for candidate in candidates:
+      print(f"{candidate}: {candidates[candidate]/total_votes:.3%} ({candidates[candidate]})")
+      
+print(f"------------------------------\n"
+      f"Winner: {winner}\n"
+      f"------------------------------\n")
 
-# analysis_txt = os.path.join("analysis", "election_analysis.txt")
-# with open(analysis_txt,"w") as txtfile:
-#     txtfile.write(f"Election Results\n"
-#       f"------------------------------\n"
-#       f"Total Votes: {total_votes}\n"
-#       f"------------------------------\n"
-#       f"{poll_data['candidates']}: {poll_data['vote_count']}% ({poll_data['vote_count']})\n"
-#       f"------------------------------\n"
-#       f"Winner: {poll_data['winner']}\n"
-#       f"------------------------------\n")
+analysis_txt = os.path.join("analysis", "election_analysis.txt")
+with open(analysis_txt,"w") as txtfile:
+    txtfile.write(f"Election Results\n"
+        f"------------------------------\n"
+        f"Total Votes: {total_votes}\n")
+    
+    for candidate in candidates:
+        txtfile.write(f"{candidate}: {candidates[candidate]/total_votes:.3%} ({candidates[candidate]})\n")
+      
+    txtfile.write(f"------------------------------\n"
+        f"Winner: {winner}\n"
+        f"------------------------------")
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # python-challenge
+
+This challenge consisted of 2 different python coding problems
+
+The Pybank problem was the first 
+    within this problem we were asked to:
+    - get total Months included in dataset
+    - The net total amount of "Profit/Loss" 
+    - Change in "Profit/Loss" over the period, and then average of those changes
+    - Greatest increase in profits 
+    - Greatest decreasee in profits
+
+The Pypoll Problem was second
+    within this problem we were asked to:
+    - get total number of votes
+    - a complete list of candidates who recieved votes
+    - the percentage of votes each candidate won
+    - the total number of votes each candidate won
+    - the winner of the election based on popular vote
+
+With each of these problems we had to code and complete the problem using the provided
+data from the csv. After we had finished coding the problems to print in the terminal
+we were asked to also output the answers to a text file in the analysis folder we created.
+
+Within my github you should see these files:
+    PyBank folder
+        analysis folder
+            profit_loss_analysis (my output of the code from main)
+        Resources folder
+            budget_data (the data supplied for the problem)
+        main (my code using python)
+
+    PyPoll folder
+        analysis folder
+            election_analysis (my output of the code from main)
+        Resources folder
+            election_data (the data supplied for the problem)
+        main (my code using python)
+
+    ReadMe
